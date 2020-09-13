d50e38242315bd3708ae5ab8bf5aeea9a4870560
Vedanth Baliga <44313631+vedanthv@users.noreply.github.com>
2020-09-13 12:15:43 +0530

    added z print script

---
+++ printz.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class printz {
+
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}
