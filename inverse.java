9caffaf9b62a116c507819f5858d0e41dec8f0a2
Vedanth Baliga <44313631+vedanthv@users.noreply.github.com>
2020-09-13 11:39:04 +0530

    added inverse number script

---
+++ inverse.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class inverse {
+
+    public static void main(String[] args) {
+        // write your code here  
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int inv = 0; //inverted digit
+        int op = 1; //op is original place
+        while (n != 0) {
+            int od = n % 10; //od is the original digit
+            int id = op; //id is the inverted digit
+            int ip = od; //ip is the inverted position
+            /*invert the digit and the position*/
+
+            //make the inverse using the logic--> id+10^(digit * inverted_place-1)
+            inv = inv + id + (int)Math.pow(10,ip-1);
+            //and then divide the no by 10
+            //now increase the original place as this acts as out counter
+            n = n / 10;
+            op++;
+        }
+        System.out.println(inv);
+    }
+}
