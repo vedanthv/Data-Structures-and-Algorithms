58b13f213e8c69e9cd34b065fbc0f08a5c7d925d
Vedanth Baliga <44313631+vedanthv@users.noreply.github.com>
2020-09-13 15:19:37 +0530

    added script to reverse a number

---
+++ reverse_number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+   
+   public class reverse_number {
+          public static void main(String[] args) {
+           Scanner scn = new Scanner(System.in);
+           int n = scn.nextInt();
+   
+           while(n != 0){
+               int digit = n % 10;
+               System.out.println(digit);
+               n = n / 10;
+           }
+       }
+   }
