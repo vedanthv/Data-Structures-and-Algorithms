0e4034ed1941dd94e19a453ab65c117b70f51628
Vedanth Baliga <44313631+vedanthv@users.noreply.github.com>
2020-09-13 12:55:48 +0530

    added is_prime_nuber solution

---
+++ is_prime_number.java
@@ -0,0 +1,29 @@
+import java.util.*;
+  
+  public class is_prime_number{
+
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int t = scn.nextInt();
+      
+      for(int i = 0; i < t; i++){
+          int n = scn.nextInt();//take input
+          /*Logic: Watch this video:
+            https://www.youtube.com/watch?v=HXSfAyjWM8A&list=PL-Jc9J83PIiFj7YSPl2ulcpwy-mwj1SSk&index=12 */
+          int div = 2;
+          while(div * div <= n){ //checks only till sqrt(number)
+              if(n % div == 0){
+                  break;   //if the divisor divides the number within the root of the number then it is not prime number
+              }
+              div++;
+          }
+          
+          if(div * div > n){
+              System.out.println("prime");
+          } else {
+              System.out.println("not prime");
+          }
+      }
+  
+   }
+  }
