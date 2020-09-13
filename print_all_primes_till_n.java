6871239c5c132dc1af90d84385ab5d9819b85e44
Vedanth Baliga <44313631+vedanthv@users.noreply.github.com>
2020-09-13 14:25:42 +0530

    Update the logic explanation

--- print_all_primes_till_n.java
+++ print_all_primes_till_n.java
@@ -6,10 +6,10 @@ public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
     int low = scn.nextInt();
     int high = scn.nextInt();
-    
+    //the outer loop runs from the low number to the high number
     for(int num = low; num <= high; num++){
         int div = 2;
-        while(div * div <= num){
+        while(div * div <= num){ //checks for  divisors from 2 to root of num = low
             if(num % div == 0){
                 break;
             }
