d998abe002d7f8cc454d79b88decda1f7b75f360
Vedanth Baliga <44313631+vedanthv@users.noreply.github.com>
2020-09-14 16:29:28 +0530

    comment to expllain functionality of the loop

--- pattern1.java
+++ pattern1.java
@@ -3,8 +3,8 @@ public class pattern1 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){ //this is row wise operation:the number of stars depends on the row no
+        for(int i=1;i<=n;i++){//job of loop is to provide uniques values of row numbers to the inner loop
+            for(int j=1;j<=i;j++){ //job is to print the stars equal to the row number i
                 System.out.print("*\t");
             }
             System.out.println(); //once it completes the inner loop this line shifts cursor to the next row
