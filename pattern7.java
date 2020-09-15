fb692827ac7fcd52d7deb0449b04beeb3a7a35d3
Vedanth Baliga <44313631+vedanthv@users.noreply.github.com>
2020-09-15 14:07:31 +0530

    added code snippet for pattern 7

---
+++ pattern7.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class pattern7 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int sp = 0;
+        int st = 1;
+
+        for(int i=1;i<= n;i++)
+        {
+            for(int j=1;j<=sp;j++)
+            {
+                System.out.print("\t");
+            }
+            for(int j=1;j<=st;j++)
+            {
+                System.out.print("*");
+            }
+            sp++;
+            System.out.println();
+        }
+
+    }
+    
+}
