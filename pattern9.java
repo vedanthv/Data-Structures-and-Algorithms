641ffd3169a5eb3eadc3301eb9d0bc7b8730cecc
Vedanth Baliga <44313631+vedanthv@users.noreply.github.com>
2020-09-15 14:43:50 +0530

    added code snippet of pattern9

---
+++ pattern9.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class pattern9{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(i==j || i+j == n+1) // the first or condition is like the pattern 7 and the second or condition is like the pattern8
+                {
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
