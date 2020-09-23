7039f32a13cf6fb9752240bd5893d00eab168dde
Vedanth Baliga <44313631+vedanthv@users.noreply.github.com>
2020-09-23 18:04:43 +0530

    734A solution added

---
+++ Codeforces/anton_and_danik.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+
+        int anton = s.length() - s.replaceAll("A", "").length();//clever use of replace all function
+        int danik = s.length() - s.replaceAll("D", "").length();
+
+        System.out.println(anton > danik ? "Anton" : anton == danik ? "Friendship" : "Danik");
+    }
+}
