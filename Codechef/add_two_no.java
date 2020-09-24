e4c47366f874308e731dd6de192c1a549636e62e
Vedanth Baliga <44313631+vedanthv@users.noreply.github.com>
2020-09-24 09:00:29 +0530

    added shorter solution

--- Codechef/add_two_no.java
+++ Codechef/add_two_no.java
@@ -1,22 +1,15 @@
 import java.util.*;
-import java.io.InputStream;
-public class Main {
-	public static void main(String[] args) {
-		// System.in and System.out are input and output streams, respectively.
-		Scanner scn = new Scanner(System.in);
-
-		// Read the number of test casese.		
-		int T = scn.nextInt();
-		while (T > 0) {
-			// Read the numbers a and b.
-			int a = scn.nextInt();
-			int b = scn.nextInt();
-			
-			// Compute the sum of a and b.
-			int ans = a + b;
-			System.out.println(ans);
-		}
-    }
+import java.io.*;
+class Main
+{  public static void main(String[] args) throws IOException
+   { Scanner sc=new  Scanner(System.in);
+       short T=sc.nextShort();
+         
+         for(short i=1;i<=T;i++)
+{  short a=sc.nextShort(); short b=sc.nextShort();
+         System.out.println(a+b);}
+   }
 }
+
 
 
