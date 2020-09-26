c7c952a362801d288f0054d4b6108c0834c7507e
Vedanth Baliga <44313631+vedanthv@users.noreply.github.com>
2020-09-26 15:12:09 +0530

    added funtionss_example-->pepcoding

---
+++ Pepcoding/Foundation/Functions and Arrays/function_example.java
@@ -0,0 +1,15 @@
+
+public class function_example {
+    public static int square(int x)
+    {
+        int sq = x * x;
+        return sq;
+    }
+    
+public static void main(String[] args)
+{
+    int square = square(4);
+    System.out.println(square);
+}
+    
+}
