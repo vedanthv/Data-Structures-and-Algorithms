8f8e6e140e01dd0dd3384fade7d32f012a1faa48
Vedanth Baliga <44313631+vedanthv@users.noreply.github.com>
2020-09-26 15:18:30 +0530

    added display function

--- Pepcoding/Foundation/Functions and Arrays/function_example.java
+++ Pepcoding/Foundation/Functions and Arrays/function_example.java
@@ -5,13 +5,17 @@ public class function_example {
         int sq = x * x;
         return sq;
     }
+    public static void display(int n)
+    {
+        System.out.println("Th square of "+n+" is "+square(n));
+    }
     
 public static void main(String[] args)
 {
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
     int square = square(n);
-    System.out.println(square);
+    display(n);
 }
     
 }
